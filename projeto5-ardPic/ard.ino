b906a6735d556fc22b517c7e54bdd0b92c3fb94a
Gabriel Bianchi <51236328+GabrielB12@users.noreply.github.com>
2022-07-25 22:45:32 -0300

    Update ard.ino

--- projeto5-ardPic/ard.ino
+++ projeto5-ardPic/ard.ino
@@ -1,9 +1,9 @@
 const int ledPin = 13; // Pino ao qual o LED será conectado
-int piscando = 0; // variável para armazenar taxa de “piscagem”
 int Control = 1;  // +++++++ VARIAVEL DE CONTROLE DA COMUNICAÇÃO ++++++++
                   // LEMBRE DE ALTERAR ESSA VARIÁVEL TAMBEM NO PIC
                   
 char incomingByte;
+String str;
 
 void setup() {
 Serial.begin(9600);
@@ -15,6 +15,25 @@ void loop()
 {
       if(Serial.available()){
           incomingByte = Serial.read();
-          Serial1.write(incomingByte);
-      }  
+          str.concat(incomingByte);
+          //Serial1.write(incomingByte);
+      }
+      if(str == "LigarLed B7"){
+          Serial1.write(1);
+      }
+      else if(str == "DesligarLed B7"){
+          Serial1.write(2);
+      }
+      else if(str == "AcionarBuzzer"){
+          Serial1.write(3);
+      } 
+     else if(str == "DesligarBuzzer"){
+          Serial1.write(4);
+      }
+     else if(str == "LigarVento"){
+          Serial1.write(5);
+      }
+     else if(str == "DesligarVento"){
+          Serial1.write(6);
+      }
      }
